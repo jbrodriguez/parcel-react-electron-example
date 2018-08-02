ae8330b3b4defa2c62b4aa4d24d82f7eb6cf75d2
rkpatel33 <mail@rishinyc.com>
2018-08-02 16:55:55 -0700

    uncomment electron remote import

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 
 // NOTE: uncomment to get fs.existsSync module error due to
 // not using target=electron
-// console.log('Accessing Electron modules!');
-// import { remote } from 'electron';
+console.log('Accessing Electron modules!');
+import { remote } from 'electron';
 
 ReactDOM.render(
   <App />,
